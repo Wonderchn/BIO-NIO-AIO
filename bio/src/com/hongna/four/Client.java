edfaff82c5c79f320102002fb9cc123e40e31ac7
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-08-04 23:19:47 +0800

    Bio传统通信实现：伪异步实现多客户端接收消息

--- bio/src/com/hongna/four/Client.java
+++ bio/src/com/hongna/four/Client.java
@@ -1,4 +1,29 @@
 package com.hongna.four;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            //1、请求与服务端的Socket对象链接
+            Socket socket = new Socket("127.0.0.1", 9999);
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            Scanner sc = new Scanner(System.in);
+            while (true){
+                System.out.println("请说：");
+                String msg = sc.nextLine();
+                ps.print(msg);
+                ps.flush();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
